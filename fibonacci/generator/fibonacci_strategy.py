74d44ab2f295e8a3b0f65d12bc26916e38bbc99f
Damian Wąsik <damianwasik98@gmail.com>
2021-05-01 17:31:40 +0200

    Added fibonacci iterative method

--- fibonacci/generator/fibonacci_strategy.py
+++ fibonacci/generator/fibonacci_strategy.py
@@ -33,9 +33,30 @@ class FibonacciRecursive(FibonacciStrategy):
 
 
 class FibonacciIterative(FibonacciStrategy):
-    pass
 
     @staticmethod
     @lru_cache(maxsize=LRU_CACHE_MAXSIZE)
     def fibonacci_result(n: int):
-        pass
+        '''
+        Simple iterative function calculating fibonacci result
+
+        :param n: fibonacci sequence term
+        :type n: int
+        :return: value of fibonacci sequence term
+        :rtype: int
+        '''
+        if n <= 1:
+            return n
+
+        two_before = 0
+        one_before = 1
+        fib_number = two_before + one_before
+
+        i = 2
+        while i < n:
+            two_before = one_before
+            one_before = fib_number
+            fib_number = two_before + one_before
+            
+            i += 1
+        return fib_number
