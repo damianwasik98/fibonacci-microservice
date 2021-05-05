9a3a58099091715347ac03e05e96520e3948f8dd
Damian Wąsik <damianwasik98@gmail.com>
2021-05-05 21:16:40 +0200

    Loading postgres env from .env

--- fibonacci/api/db.py
+++ fibonacci/api/db.py
@@ -1,7 +1,18 @@
+import os
+
 import databases
 import sqlalchemy
+from dotenv import load_dotenv
 
-DATABASE_URL = 'postgresql://fibonacci:fibonacci@localhost:5432/fibonacci_sequence'
+load_dotenv('.env')
+
+postgres_host = os.environ['POSTGRES_HOST']
+postgres_port = os.environ['POSTGRES_PORT']
+postgres_db = os.environ['POSTGRES_DB']
+postgres_user = os.environ['POSTGRES_USER']
+postgres_pass = os.environ['POSTGRES_PASS']
+
+DATABASE_URL = f'postgresql://{postgres_user}:{postgres_pass}@{postgres_host}:{postgres_port}/{postgres_db}'
 
 database = databases.Database(DATABASE_URL)
 
