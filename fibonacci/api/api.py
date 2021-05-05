1721aeb944e7cb72e76ca0eeb107e531c23c7018
Damian Wąsik <damianwasik98@gmail.com>
2021-05-05 20:44:46 +0200

    Added api fibonacci endpoint and defined fibonacci schema

---
+++ fibonacci/api/api.py
@@ -0,0 +1,21 @@
+from typing import List
+
+from fastapi import FastAPI
+
+from db import database
+import schema
+
+app = FastAPI(title='Fibonacci API')
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+@app.get("/fibonacci/{number}", response_model=schema.Fibonacci)
+async def fibonacci(number: int):
+    query = Fibonacci.select().where(Fibonacci.c.number == number)
+    return await database.fetch_one(query)
